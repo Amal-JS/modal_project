303976fc9a621b41be5f9f61d9e09536eac762ea
amaljs <amaljs121@gmail.com>
2023-08-16 20:43:04 +0530

    added_comments_changed_ui

--- modalproject/modalapp/views.py
+++ modalproject/modalapp/views.py
@@ -13,32 +13,27 @@ def page1(request):
     return render(request,'simple.html')
 
 def create_account_js(request):
-    
-    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':  #checking if request is of type XMLHttpRequest  we are using fetch api but setting XMLHttpRequest in header
         form = Custom_User_Form(request.POST)
         if form.is_valid():
             form.save()
             print('accepted')
-            return JsonResponse({'success': 'True'})
+            return JsonResponse({'success': 'True'})  #sending response as json with object in success key value 'True'
             
         else:
+            errors = {field_name: form.errors[field_name] for field_name in form.errors} #if any error we are creating a dictionary with field name and errors in each field field_name : [error1,erro2,...]
+            res=JsonResponse({'success':'False', 'errors': errors}) #sending  that error along with success set to 'False' and errors value set to errors dictionary
             
-            
-            
-
-            errors = {field_name: form.errors[field_name] for field_name in form.errors}
-            res=JsonResponse({'success':'False', 'errors': errors})
-            print(res,"vannilla")
             return res
-    else:
-        form = Custom_User_Form()
+   
 
         
 
     
     
 
-
+#django way
 
 def create_account(request):
     
--- modalproject/static/js/modal.js
+++ modalproject/static/js/modal.js
@@ -1,84 +1,91 @@
-console.log('modal js')
+// all js codes handles form submission
 
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { //event to ensure that your JavaScript code runs only when the HTML document has finished loading.
     const form = document.querySelector('#myModal form');
 
-    form.addEventListener("submit", async function (event) {
+    form.addEventListener("submit", async function (event) { //submit event on form
         event.preventDefault();
-    
 
-        
-    try {
-        const formData = new FormData(form);
-        const response = await fetch(createAccountUrl , {
-            method: 'POST',
-            body: new URLSearchParams(new FormData(form)),
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
+
+
+        try {
+            const formData = new FormData(form);
+            const response = await fetch(createAccountUrl, { //createAccount is in m.html , using await after the fetch returns the response it will not be executed
+                method: 'POST',
+                body: new URLSearchParams(new FormData(form)),
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            });
+
+            const responseData = await response.json(); // Parse the JSON response
+            clearErrors(); // Clear any existing error messages
+            successFunction(responseData); // Call successFunction with the parsed JSON response
+        } catch (error) {
+            console.log('Error:', error);
+        }
+
+    });
+
+
+
+
+});
+
+function successFunction(msg) {  //msg contains the respone in json form
+
+    if (msg.success === 'True') {  //if success
+        console.log("success")
+        const form = document.querySelector('#myModal form');
+        form.reset(); //reset form empty
+        window.location.href = page1Url; //redirect to success url
+
+    } else if (msg.success === 'False') {  //form data invalid
+
+        console.log('error  coming', msg.errors, "ended")
+
+        const errorMessages = msg.errors;  //get the error object to a variable
+
+        for (const fieldName in errorMessages) {
+            const field = document.querySelector(`[name="${fieldName}"]`); // getting the input tag with name eg : username == ${fieldName}
+
+            if (field) {  //check if that input  element exist in html
+
+                const errorMsgs = errorMessages[fieldName]; // Array of error messages
+
+                const errorContainer = document.createElement('div'); //create a new div
+                errorContainer.className = 'error'; //adding a class to this new div
+
+                // Loop through the array of error messages and create separate lines
+                for (const errorMsg of errorMsgs) {
+                    const errorLine = document.createElement('p'); //create a new p element
+                    errorLine.textContent = errorMsg; //adding the error each on p element
+                    errorLine.classList.add('text-danger'); // adding a class to the p element
+                    errorContainer.appendChild(errorLine); //adding that p to div element
+
+                }
+
+                // Find the error container associated with the field and append errors
+                const errorId = `${fieldName}-error`; //getting the div given in html to display error for each field
+                const fieldErrorContainer = document.getElementById(errorId);
+                if (fieldErrorContainer) {
+                    fieldErrorContainer.innerHTML = ''; // Clear existing errors
+                    fieldErrorContainer.appendChild(errorContainer);
+                }
             }
-        });
-
-        const responseData = await response.json(); // Parse the JSON response
-        clearErrors(); // Clear any existing error messages
-        successFunction(responseData); // Call successFunction with the parsed JSON response
-    } catch (error) {
-        console.log('Error:', error);
+        }
     }
-            
-        });
 
 
-        
-            
-        });
+}
 
-        function successFunction(msg) {
-                                       //setting both div to empty
-        //const container_email = document.getElementById('er_email');
-        //container_email.innerHTML = "";
-    
-        if (msg.success === 'True') {
-            console.log("success")
-            const form = document.querySelector('#myModal form');
-            form.reset(); //reset form empty
-            window.location.href = page1Url; //redirect to success url
 
-        } else if (msg.success === 'False') {
 
-          console.log('error  coming',msg.errors,"ended")
-          const errorMessages = msg.errors;  //get the error object to a variable
-          for (const fieldName in errorMessages) {
-          const field = document.querySelector(`[name="${fieldName}"]`);
 
-        if (field) {
 
-            const errorMsgs = errorMessages[fieldName]; // Array of error messages
-
-            const errorContainer = document.createElement('div');
-            errorContainer.className = 'error';
-
-            // Loop through the array of error messages and create separate lines
-            for (const errorMsg of errorMsgs) {
-                const errorLine = document.createElement('p');
-                errorLine.textContent = errorMsg;
-                errorLine.classList.add('text-danger'); 
-                errorContainer.appendChild(errorLine);
-                
-            }
-
-            field.insertAdjacentElement('afterend', errorContainer);
-        }}
-        
-    
-    }
-    
-        
-    }
-    
-
-    function clearErrors() {
-    const errorContainers = document.querySelectorAll('.error');  //fu
+function clearErrors() {
+    const errorContainers = document.querySelectorAll('.error');  //fuction to clear errors when re submitting the form
     errorContainers.forEach(container => container.remove());
 }
 
